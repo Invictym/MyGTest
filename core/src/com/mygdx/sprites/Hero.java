bc30093d116697d66385f2b9ceef65ee2309121f
akalatsky <a.v.kalatskiJ@gmail.com>
2018-10-02 18:19:22 +0300

    Work with animation

--- core/src/com/mygdx/sprites/Hero.java
+++ core/src/com/mygdx/sprites/Hero.java
@@ -1,6 +1,7 @@
 package com.mygdx.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector3;
 
@@ -9,14 +10,19 @@ public class Hero {
     private Vector3 velocity;
 
     private Texture hero;
-    private TextureRegion heroMainRegion;
+    private TextureRegion heroRegion;
+    private TextureRegion[][] heroTextureRegions;
+    private Animation herAnim;
     private static final int GRAVITY = -10;
+    private boolean isMove = false;
 
     public Hero(int x, int y) {
         this.position = new Vector3(x, y, 0);
         this.velocity = new Vector3(0, 0 , 0);
         hero = new Texture("gladiator_arena_sprites.gif");
-        heroMainRegion = TextureRegion.split(hero, hero.getWidth() / 6, hero.getHeight() / 6)[0][0];
+        heroTextureRegions = TextureRegion.split(hero, hero.getWidth() / 6, hero.getHeight() / 6);
+        heroRegion = heroTextureRegions[0][0];
+        herAnim = new Animation(0.025f, heroTextureRegions[0][1], heroTextureRegions[0][2], heroTextureRegions[0][3], heroTextureRegions[0][4]);
     }
 
     public void update(float dt) {
@@ -24,6 +30,12 @@ public class Hero {
         velocity.scl(dt);
         position.add(velocity.x, velocity.y, 0);
         velocity.scl(1 / dt);
+
+        if (isMove) {
+            heroRegion = (TextureRegion) herAnim.getKeyFrame(dt);
+        } else {
+
+        }
 
         if (position.y < 0) {
             position.y = 0;
@@ -39,15 +51,23 @@ public class Hero {
     }
 
     public TextureRegion getHeroMainRegion() {
-        return heroMainRegion;
+        return heroRegion;
     }
 
     public void moveRight() {
         velocity.x = 100;
+        if (heroRegion.isFlipX()) {
+            heroRegion.flip(true, false);
+        }
+        isMove = true;
     }
 
     public void moveLeft() {
         velocity.x = -100;
+        if (!heroRegion.isFlipX()) {
+            heroRegion.flip(true, false);
+        }
+        isMove = true;
     }
 
     public void jump() {
@@ -58,5 +78,6 @@ public class Hero {
 
     public void stop() {
         velocity.x = 0;
+        isMove = false;
     }
 }
